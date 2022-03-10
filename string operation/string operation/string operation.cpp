ad4c01c7cc7a89d4924ffac7bd67f389367eeced
ReVuz <98228399+ReVuz@users.noreply.github.com>
2022-03-10 22:38:18 +0530

    lab cycle-1

--- string operation/string operation/string operation.cpp
+++ string operation/string operation/string operation.cpp
@@ -1,30 +1,101 @@
-#include<iostream>
+#include <iostream>
+#include <cstring>
+
 using namespace std;
-void len(string s) {
-	int i;
-	for (i = 0; s[i]; i++)
-		;
-		cout << i << endl;
-}
-int main() {
-	string s1, s2, s3;
-	cout << "Enter any two words "<<endl;
-	cin >> s1 >> s2;
-	if (s1 == s2)
-		cout << "Both are same " << endl;
-	else
-		cout << "Both are different " << endl;
-	s3 = s1 + s2;
-	cout << s1 << " + " << s2 <<" = " << s3 << endl;
-	if (s1 > s2)
-	{
-		cout << s1 << " is greater in length" << endl;
-		cout << "Length of " << s1 << " = "; len(s1);
-		cout<< endl;
+
+class String
+{
+	char* str;
+	int len;
+public:
+	String() {
+		len = 0;
+		str = new char[len + 1];
 	}
+	String(const char* s)
+	{
+		len = strlen(s);
+		str = new char[len + 1];
+		strcpy(str, s);
+	}
+	void input() { cin >> str; }
+	void add(String a, String b);
+	void get_len();
+	void compare(String s1, String S2);
+};
+
+void String::add(String a, String b)
+{
+	len = a.len + b.len;
+	delete str;
+	str = new char[len + 1];
+	strcpy(str, a.str);
+	strcat(str, b.str);
+	cout << str;
+}
+
+void String::get_len()
+{
+	cout << strlen(str);
+}
+
+void String::compare(String s1, String s2)
+{
+	int m, n;
+	m = strlen(s1.str);
+	n = strlen(s2.str);
+	if (m <= n)
+		cout << s1.str << " is smaller than " << s2.str << "\n";
 	else
-		cout << s2 << " is greater in length" << endl;
-	cout << "Length of " << s2 << " = "; len(s2);
-	cout<< endl;
+		cout << s1.str << " is smaller than " << s2.str << "\n";
+}
+
+int main()
+{
+	String Str1, Str2, Str3;
+	int ans;
+	do {
+		cout << "String Operations "
+			"\nHere are some of the options available : "
+			"\n1. Concatenation of string "
+			"\n2. Finding the length of both the strings  "
+			"\n3. Finding the length of concatenated string"
+			"\n4. Compare the strings"
+			"\n5. Exit "
+			"\n Choose any number ";
+		int num;
+		cin >> num;
+		if (num == 5)
+		{
+			;
+			break;
+		}
+		cout << "Enter the first string : ";
+		Str1.input();
+		cout << "\nEnter second string : ";
+		Str2.input();
+		if (num == 1) {
+			cout << "Concatenating the two strings gives : ";
+			Str3.add(Str1, Str2);
+		}
+		if (num == 2) {
+			cout << "\nlen of first string : \n";
+			Str1.get_len();
+			cout << "\nlen of second string : ";
+			Str2.get_len();
+		}
+		if (num == 3) {
+			cout << "\nlen of concatnated string : ";
+			Str3.get_len();
+		}
+		if (num == 4) {
+			cout << "Comparison of string : ";
+			Str3.compare(Str1, Str2);
+		}
+		cout << "\nDo you want to continue ? \nIf yes,press 1 "
+			"\nElse press 0 : ";
+		cin >> ans;
+	} while (ans);
+	cout << "Thank you,See again ";
 	return 0;
 }

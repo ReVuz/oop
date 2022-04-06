a8f5dcf4d83c17b6369cca4639ab32c5363bd144
ReVuz <98228399+ReVuz@users.noreply.github.com>
2022-04-06 11:14:20 +0530

    Update string operation.cpp

--- string operation/string operation/string operation.cpp
+++ string operation/string operation/string operation.cpp
@@ -45,17 +45,22 @@ void String::compare(String s1, String s2)
 	m = strlen(s1.str);
 	n = strlen(s2.str);
 	if (m <= n)
-		cout << s1.str << " is smaller than " << s2.str << "\n";
+		cout << s1.str << " is smaller than " << s2.str << endl;
 	else
-		cout << s1.str << " is smaller than " << s2.str << "\n";
+		cout << s1.str << " is smaller than " << s2.str << endl;
 }
 
 int main()
 {
 	String Str1, Str2, Str3;
 	int ans;
+		cout << "Enter the first string : ";
+		Str1.input();
+		cout << "\nEnter second string : ";
+		Str2.input();
 	do {
-		cout << "String Operations "
+		
+		cout << "\n\nString Operations "
 			"\nHere are some of the options available : "
 			"\n1. Concatenation of string "
 			"\n2. Finding the length of both the strings  "
@@ -65,37 +70,31 @@ int main()
 			"\n Choose any number ";
 		int num;
 		cin >> num;
-		if (num == 5)
-		{
-			;
-			break;
-		}
-		cout << "Enter the first string : ";
-		Str1.input();
-		cout << "\nEnter second string : ";
-		Str2.input();
 		if (num == 1) {
 			cout << "Concatenating the two strings gives : ";
 			Str3.add(Str1, Str2);
 		}
-		if (num == 2) {
+		else if (num == 2) {
 			cout << "\nlen of first string : \n";
 			Str1.get_len();
 			cout << "\nlen of second string : ";
 			Str2.get_len();
 		}
-		if (num == 3) {
+		else if (num == 3) {
 			cout << "\nlen of concatnated string : ";
 			Str3.get_len();
 		}
-		if (num == 4) {
+		else if (num == 4) {
 			cout << "Comparison of string : ";
 			Str3.compare(Str1, Str2);
 		}
+		else
+		  break;
+		  
 		cout << "\nDo you want to continue ? \nIf yes,press 1 "
 			"\nElse press 0 : ";
 		cin >> ans;
 	} while (ans);
-	cout << "Thank you,See again ";
+	cout << "\nThank you,See again "<<endl;
 	return 0;
 }

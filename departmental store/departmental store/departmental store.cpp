3ad3a6c74b0098361e71d9f7a38265723f7bd510
ReVuz <98228399+ReVuz@users.noreply.github.com>
2022-04-08 01:39:16 +0530

    Update departmental store.cpp

--- departmental store/departmental store/departmental store.cpp
+++ departmental store/departmental store/departmental store.cpp
@@ -5,6 +5,7 @@ class item {
 	string  itemname[m];
 	int itemcode[m];
 	float itemprice[m];
+	int itemquantity[m];
 	int count;
 public:
 	void cnt(void) { count = 0; }
@@ -14,83 +15,89 @@ public:
 	void buyitem(void);
 };
 void item::getitem(void) {
-    int ad;
-    cout<<"How many items do you want to add : ";
-    cin>>ad;
-    for (int i=0;i<ad;i++){
-    	cout<<"Item "<<i+1<<endl; 
-	cout << "Enter item code : ";
-	cin >> itemcode[count];
-	
-	cout<<"Enter item name : ";
-	cin>>itemname[count];
+	int ad;
+	cout << "How many items do you want to add : ";
+	cin >> ad;
+	for (int i = 0; i < ad; i++) {
+		cout << "Item " << i + 1 << endl;
+		cout << "Enter item code : ";
+		cin >> itemcode[count];
 
-	cout << "Enter item cost : ";
-	cin >> itemprice[count];
-	count++;
-    }
+		cout << "Enter item name : ";
+		cin >> itemname[count];
+
+		cout << "Enter item cost : ";
+		cin >> itemprice[count];
+
+		cout << "Enter item quantity : ";
+		cin >> itemquantity[count];
+		count++;
+	}
 }
 void item::buyitem(void) {
 	float sum = 0;
-	int i,num;
-	cout<<"Enter the number of items : ";
-	cin>>num;
+	int i, num,q;
+	cout << "Enter the number of items : ";
+	cin >> num;
 	int code[num];
-	for (i = 0; i < num; i++){
-		cout<<"Enter the itemcode which you want to buy : ";
-		cin>>code[i];
-		for(int j=0;j<count;j++){
-	    		if(code[i] == itemcode[j]){
-	     			sum = sum + itemprice[j];	
-	    		}
-	    	}
-	}
-	cout << "Name \tCode \tCost "<<endl;
-	for (i = 0; i < num; i++){
-		for(int j=0;j<count;j++){
-			if(code[i] == itemcode[j]){
-			cout<<itemname[j]<<"\t"<<itemcode[j]
-			<<"\t"<<itemprice[j]<<endl;
+	for (i = 0; i < num; i++) {
+		cout << "Enter the itemcode which you want to buy : ";
+		cin >> code[i];
+		for (int j = 0; j < count; j++) {
+			if (code[i] == itemcode[j]) {
+				cout << "Enter the item quantity : ";
+				cin >> q;
+				sum = sum + itemprice[j]*q;
+				itemquantity[j] = itemquantity[j] - q;
 			}
 		}
 	}
-	cout<<"Total bill : "<<sum;
+	cout << "Name \tCode \tQuantity \tCost " << endl;
+	for (i = 0; i < num; i++) {
+		for (int j = 0; j < count; j++) {
+			if (code[i] == itemcode[j]) {
+				cout << itemname[j] << "\t" << itemcode[j]
+					<< "\t" <<q<<"\t\t" << itemprice[j]*q << endl;
+			}
+		}
+	}
+	cout << "Total bill : " << sum;
 }
 void item::itemremove(void) {
 	int a;
 	cout << "Enter item code : ";
 	cin >> a;
 	int i;
-	for (i = 0; i < count; i++){
-		if (itemcode[i] == a){
-		    for(int j=a;j<count;j++){
-			itemprice[i] = itemprice[i+1];
-			itemname[i]=itemname[i+1];
-			itemcode[i]=itemcode[i+1];
-		        
-		    }
-		    count--;
-		    cout<<"Item removed successfully "<<endl;
-		    break;
+	for (i = 0; i < count; i++) {
+		if (itemcode[i] == a) {
+			for (int j = a; j < count; j++) {
+				itemprice[i] = itemprice[i + 1];
+				itemname[i] = itemname[i + 1];
+				itemcode[i] = itemcode[i + 1];
+				itemquantity[i] = itemquantity[i + 1];
+			}
+			count--;
+			cout << "Item removed successfully " << endl;
+			break;
 		}
 		else
-	      cout<<"Item not found "<<endl;
-	      break;
+			cout << "Item not found " << endl;
+		break;
 	}
-	
+
 }
 void item::displayitems(void) {
-    cout << "\n" << "SL nO \tCode \tName \tPrice";
+	cout << "\n" << "SL nO \tCode \tName \tQuantity \tPrice";
 	int i;
 	for (i = 0; i < count; i++) {
-		cout << "\n" <<i+1<<"\t"<<itemcode[i]<<"\t"<<itemname[i]<<"\t"<< itemprice[i];
+		cout << "\n" << i + 1 << "\t" << itemcode[i] << "\t" << itemname[i] << "\t" <<itemquantity[i]<<"\t\t"<< itemprice[i];
 	}
 	cout << endl;
 }
 int main() {
 	item s;
 	s.cnt();
-	int x,ans;
+	int x, ans;
 	do {
 		cout << "\n\tABC Departmental Store "
 			<< "\n1. Add an item \n2. Buy an item "
@@ -106,9 +113,11 @@ int main() {
 		case 5: break;
 		default:cout << "Error 404! \tNot found ";
 		}
-		cout<<"\nDo you want to continue ?(1/0) ";
-		cin>>ans;
-			
+		if(x!=5){
+		cout << "\nDo you want to continue ?(1/0) ";
+		cin >> ans;
+		}
+
 	} while (ans);
 	cout << "Thank you  see again";
 	return 0;
